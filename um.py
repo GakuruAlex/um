6e64d03c2ca308649c5228f8040d5383797b4f91
= <=>
2025-01-20 11:10:59 +0300

    using findall to find occurrence of um

--- um.py
+++ um.py
@@ -1,6 +1,10 @@
+from re import IGNORECASE, findall
 def count(text: str)-> int:
-    pass
+    pattern = r'(\bum\b)+'
+    return len(findall(pattern=pattern, string= text, flags=IGNORECASE))
 def main()-> None:
+    # test_text = 'hello world, this food is UM yummy'
+    # print(f"um appears {count(test_text)} times. Expecting 1")
     print(count(input("Text: ")))
 
 if __name__ == "__main__":
