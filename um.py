da9e2bd848f9e5c9818e8d7d135562cb12694915
= <=>
2025-01-20 11:14:15 +0300

    docstr

--- um.py
+++ um.py
@@ -1,5 +1,19 @@
 from re import IGNORECASE, findall
 def count(text: str)-> int:
+    """_Find the number of times um occurs standalone in a string but not as a substring_
+
+    Args:
+        text (str): _Input string_
+
+    Returns:
+        int: _Number times um occurs in str_
+    
+    Example:
+        >>> count("Hello world, this food is um yummy")
+            1
+        >>> count("Hello world")
+            0
+    """
     pattern = r'(\bum\b)+'
     return len(findall(pattern=pattern, string= text, flags=IGNORECASE))
 def main()-> None:
