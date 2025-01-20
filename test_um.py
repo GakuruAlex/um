8512d94daf56f544c2e854a544c529168709efb5
= <=>
2025-01-20 10:05:18 +0300

    testing um appearing at end of string

--- test_um.py
+++ test_um.py
@@ -11,7 +11,17 @@ def test_um_for_an_empty_text(text, um_count):
 @pytest.mark.parametrize("text, um_count",[
     ("um, um, um, hello, world", 3),
     ("um , um , um , hello, world", 3),
-    ("um um um  hello world", 3)
+    ("um um um  hello world", 3),
+    ("um hello world", 1),
+    ("um, hello world", 1),
 ])
 def test_um_appearing_at_the_start_of_the_string(text, um_count):
+    assert count(text) == um_count
+
+@pytest.mark.parametrize("text, um_count", [
+    ("Hello world, um", 1),
+    ("hello, world um, um", 2),
+    ("hello world, um um ", 2)
+])
+def test_um_appearing_at_end_of_string(text, um_count):
     assert count(text) == um_count
